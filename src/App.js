841d3ad3a81479e9a340f6b509cd9179eee73d3f
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-08-04 00:50:46 -0400

    Adjust details

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import { useState, useEffect } from 'react'
-import './App.css';
-import Gallery from './Components/Gallery'
-import ButtonBar from './Components/ButtonBar'
+import { useState, useEffect } from "react"
+import "./App.css"
+import Gallery from "./Components/Gallery"
+import ButtonBar from "./Components/ButtonBar"
 
 function App() {
-  let [objectId, setObjectId] = useState(12770)
+  let [artId, setArtId] = useState(12720)
   let [data, setData] = useState({})
 
   useEffect(() => {
-    document.title='Welcome to ArtWorld'
-    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
-    .then(response => response.json())
-    .then(resdata => setData(resdata))
-  }, [objectId])
+    document.title = "Mike\'s ArtWorld"
+    fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`
+    )
+      .then((response) => response.json())
+      .then((resdata) => setData(resdata))
+  }, [artId])
 
   const handleIterate = (e) => {
-    setObjectId(objectId + Number(e.target.value))
+    setArtId(artId + Number(e.target.value))
   }
 
   const displayImage = () => {
-    if(!data.primaryImage) {
-      return (
-        <h2>No Image!</h2>
-      )
+    if (!data.primaryImage) {
+      return <h2>No Image!</h2>
     }
     return (
-      <Gallery objectImg={data.primaryImage} title={data.title} artist={data.artistDisplayName} medium={data.medium} date={data.objectDate}/>
+      <Gallery
+        objectImg={data.primaryImage}
+        title={data.title}
+        artist={data.artistDisplayName}
+        medium={data.medium}
+        date={data.objectDate}
+      />
     )
   }
 
   return (
     <div className="App">
       <h1>{data.title}</h1>
-      <div style={{'width': '100%'}}>
-        {displayImage()}
-      </div>
+      <div style={{ width: "100%" }}>{displayImage()}</div>
       <ButtonBar handleIterate={handleIterate} />
     </div>
-  );
+  )
 }
 
 export default App
